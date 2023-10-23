969048138a86335d011c54e9d0364cd477e8db18
3x10RaiseTo8 <abhisheks1177@gmail.com>
2023-10-23 17:54:56 +0530

    Finish ex2.19 and 2.20, finish part2

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import getAllCountries from "./services/countriesServices";
+import countriesServices from "./services/countriesServices";
 
 const SearchBar = ({ label, handleChange, value }) => {
   return (
@@ -11,35 +11,61 @@ const SearchBar = ({ label, handleChange, value }) => {
 };
 
 const Country = ({ country }) => {
+  const [weatherInfo, setWeatherInfo] = useState(null);
+  // Get weather
+  useEffect(() => {
+    countriesServices
+      .getWeather(country.capital)
+      .then((r) => setWeatherInfo(r.data))
+      .then(console.log("Retrieved Weather"))
+      .catch((error) => console.error("Err loading weather effect:", error));
+  }, []);
+
   return (
     <>
       <h2>{country.name.common}</h2>
-      <p>Capital: {country.capital}</p>
-      <p>Area: {country.area}</p>
-      <p>Continent: {country.continents}</p>
+      <div>Capital: {country.capital}</div>
+      <div>Area: {country.area}</div>
+      <div>Continent: {country.continents}</div>
       <h3>Languages</h3>
       <ul>
         {Object.values(country.languages).map((lang) => (
           <li key={lang}>{lang}</li>
         ))}
       </ul>
-      <h3>Map</h3>
+      <h3>Flag</h3>
       <img
         src={country.flags.svg}
         alt={country.name.common + "flag"}
         style={{ width: "200px" }}
       />
+      {weatherInfo && (
+        <div>
+          <h3>Weather in {country.capital}</h3>
+          <div>
+            Temperature: {(weatherInfo.main.temp - 273.15).toFixed(2)} degree
+            celsius
+          </div>
+          <img
+            src={countriesServices.getWeatherIconSrc(
+              weatherInfo.weather[0].icon
+            )}
+            alt={weatherInfo.weather[0].description}
+          />
+          <div>{weatherInfo.weather[0].description}</div>
+          <div>Wind: {weatherInfo.wind.speed} m/s</div>
+        </div>
+      )}
     </>
   );
 };
 
-const SearchResult = ({ countries, filter }) => {
+const SearchResult = ({ countries, filter, handleShow }) => {
   // Filter logic
   const lowerCaseFilter = filter.toLowerCase();
   const filteredCountries = countries.filter((country) =>
     country.name.common.toLowerCase().includes(lowerCaseFilter)
   );
-  console.log(filteredCountries[0]);
 
   if (filteredCountries.length > 10 && lowerCaseFilter) {
     return <div>Too many matches. Specify another Filter</div>;
@@ -49,7 +75,12 @@ const SearchResult = ({ countries, filter }) => {
     return (
       <div>
         {filteredCountries.map((country) => (
-          <div key={country.name.common}>{country.name.common}</div>
+          <div key={country.name.common}>
+            {country.name.common}{" "}
+            <button onClick={() => handleShow(country.name.common)}>
+              Show
+            </button>
+          </div>
         ))}
       </div>
     );
@@ -61,11 +92,13 @@ const App = () => {
 
   const handleFilterChange = (event) => setNewFilter(event.target.value);
 
+  const handleShow = (country) => setNewFilter(country);
+
   useEffect(() => {
-    getAllCountries().then((response) => {
-      setCountries(() => response.data);
-    });
-    console.log("Effect success", countries);
+    countriesServices
+      .getAllCountries()
+      .then((response) => setCountries(() => response.data))
+      .then(console.log("Retrieved countries"));
   }, []);
 
   return (
@@ -75,7 +108,11 @@ const App = () => {
         value={newFilter}
         handleChange={handleFilterChange}
       />
-      <SearchResult countries={countries} filter={newFilter} />
+      <SearchResult
+        countries={countries}
+        filter={newFilter}
+        handleShow={handleShow}
+      />
     </>
   );
 };
--- part2/countries/src/services/countriesServices.js
+++ part2/countries/src/services/countriesServices.js
@@ -1,8 +1,16 @@
 import axios from "axios";
 
-const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api";
 // const baseUrl = "http://localhost:3001";
+
+const appid = import.meta.env.VITE_SOME_KEY;
+const baseUrl = "https://studies.cs.helsinki.fi/restcountries/api";
+const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${appid}`;
+const weatherIconUrl = "https://openweathermap.org/img/wn";
 
 const getAllCountries = () => axios.get(`${baseUrl}/all`);
 
-export default getAllCountries;
+const getWeather = (city) => axios.get(`${weatherUrl}&q=${city}`);
+
+const getWeatherIconSrc = (code) => `${weatherIconUrl}/${code}@2x.png`;
+
+export default { getAllCountries, getWeather, getWeatherIconSrc };
