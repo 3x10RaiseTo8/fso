1f541b221c035bb6c4a4f3a18c5b189a5c3c03ed
3x10RaiseTo8 <abhishekcy17@gmail.com>
2024-01-06 02:32:40 +0530

    Add user route to the main app

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const app = express();
 require('express-async-errors');
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
+const userRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -27,6 +28,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', userRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
