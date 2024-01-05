b350b4cf34e43805c91702d5b8a99982309cb554
3x10RaiseTo8 <abhishekcy17@gmail.com>
2024-01-06 05:21:46 +0530

    Establish connection between users and blogs

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,25 +1,39 @@
 const blogsRouter = require('express').Router();
+const User = require('../models/user');
 const Blog = require('../models/blog');
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate('user', { name: 1, username: 1 });
   response.json(blogs);
 });
 
 blogsRouter.post('/', async (request, response) => {
   const { title, author, url, likes = 0 } = request.body;
+
+  const users = await User.find({});
+  const id = users[0].id;
+  const user = await User.findById(id);
+
   const blogObject = new Blog({
     title,
     author,
     url,
     likes,
+    user: user.id,
   });
+
   const savedBlog = await blogObject.save();
+  user.blogs.concat(savedBlog._id);
+  await user.save();
+
   response.status(201).json(savedBlog);
 });
 
 blogsRouter.get('/:id', async (request, response) => {
-  const blog = await Blog.findById(request.params.id);
+  const blog = await Blog.findById(request.params.id).populate('user', {
+    name: 1,
+    username: 1,
+  });
   if (blog) {
     response.json(blog);
   } else {
