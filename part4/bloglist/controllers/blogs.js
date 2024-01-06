2dfc5938736dc1babc3c4f4cfeff8d30dde985ac
3x10RaiseTo8 <abhishekcy17@gmail.com>
2024-01-07 01:36:37 +0530

    Add user authorization for delete blog request

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -11,7 +11,6 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const { title, author, url, likes = 0 } = request.body;
 
-  console.log(request.token);
   const decodedToken = jwt.verify(request.token, process.env.SECRET);
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'invalid token' });
@@ -38,16 +37,24 @@ blogsRouter.get('/:id', async (request, response) => {
     name: 1,
     username: 1,
   });
-  if (blog) {
-    response.json(blog);
-  } else {
+  if (!blog) {
     response.status(404).end();
   }
+  response.json(blog);
 });
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id);
-  response.status(204).end();
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'invalid token' });
+  }
+  const blog = await Blog.findById(request.params.id);
+  const user = await User.findById(decodedToken.id);
+  if (blog.user.toString() !== user.id.toString()) {
+    return response.status(403).json({ error: 'invalid token' });
+  }
+  await Blog.findByIdAndDelete(blog.id);
+  return response.status(204).end();
 });
 
 blogsRouter.put('/:id', async (request, response) => {
