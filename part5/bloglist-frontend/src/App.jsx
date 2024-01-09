1f6af2aee421e40dd2f656d17434923a45fce8d4
3x10RaiseTo8 <abhishekcy17@gmail.com>
2024-01-10 04:10:06 +0530

    Toggle BlogForm after a new blog is created

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,17 +1,23 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Blog from './components/Blog';
 import LoginForm from './components/LoginForm';
 import BlogForm from './components/BlogForm';
 import Notification from './components/Notification';
 import blogService from './services/blogs';
 import loginService from './services/login';
+import Togglable from './components/Togglable';
 
 const App = () => {
+  // States
   const [blogs, setBlogs] = useState([]);
   const [user, setUser] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
   const [successMessage, setSuccessMessage] = useState(null);
 
+  // Refs
+  const noteFormRef = useRef();
+
+  // Event Handlers
   const handleLogout = () => {
     window.localStorage.removeItem('loggedInUser');
     setUser(null);
@@ -41,6 +47,7 @@ const App = () => {
     try {
       const response = await blogService.createBlog(blogObject);
       setBlogs(blogs.concat(response));
+      noteFormRef.current.toggleVisibility();
       setSuccessMessage(
         `A new blog "${response.title}" by "${
           response.author || 'Unknown'
@@ -57,6 +64,7 @@ const App = () => {
     }
   };
 
+  // Effects
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
@@ -70,6 +78,7 @@ const App = () => {
     }
   }, []);
 
+  // Render the components
   if (!user) {
     return (
       <div>
@@ -89,15 +98,23 @@ const App = () => {
         <Notification message={successMessage} className="success" />
       )}
 
-      <BlogForm createBlog={createBlog} />
-      <h2>Blogs</h2>
+      <h1>Blogs</h1>
+
       {user && (
         <div>
           {user.name} is logged in
           <button onClick={handleLogout}>Logout</button>
         </div>
       )}
-      <hr></hr>
+
+      <hr />
+
+      <Togglable buttonLabel="Create New Blog" ref={noteFormRef}>
+        <BlogForm createBlog={createBlog} />
+      </Togglable>
+
+      <hr />
+
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
