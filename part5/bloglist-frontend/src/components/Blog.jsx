c74c7c9fdda6a8d10b05c725e4cb7b206e75ae9c
3x10RaiseTo8 <abhishekcy17@gmail.com>
2024-01-10 05:42:43 +0530

    Make Blog to show more info on view

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - {blog.author}
-  </div>
-);
+import { useState } from 'react';
+
+const Blog = ({ blog, handleLike }) => {
+  const [view, setView] = useState(false);
+
+  const label = view ? 'Hide' : 'Show';
+
+  const handleView = () => {
+    console.log(view, label);
+    setView(!view);
+  };
+
+  return (
+    <div>
+      <p>
+        {blog.title} - {blog.author}{' '}
+        <button onClick={handleView}>{label}</button>
+      </p>
+      {view && (
+        <div>
+          <p>
+            <a href={blog.url}>{blog.url}</a>
+          </p>
+          <p>
+            Likes {blog.likes} <button onClick={handleLike}>Like</button>
+          </p>
+          <p>Added by {blog.user.name}</p>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
